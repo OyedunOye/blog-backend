dae903a7a5858b2c274e47f12e32d398b0abdb7b
OyedunOye <oyesinaoyedun@yahoo.com>
2025-08-04 23:56:25 +0200

    feat: added /auth/google and /google/callback endpoints to perform the authentication by google.

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs')
 const config = require('../utils/config');
 const { generateOTP } = require("../utils/generateOtp");
 const handleSendEmail = require('../utils/handleSendEmail');
+const passport = require('passport')
 
 // console.log(new Date(Date.now()))
 // console.log(new Date)
@@ -47,6 +48,48 @@ loginRouter.post("/", async (req, res) => {
         res.status(200).json({token: token, message: "Logged in successfully!"})
 
     }
+})
+
+// Google OAuth starts here
+loginRouter.get("/auth/google", passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+// callback after Google authenticates the user
+loginRouter.get('/google/callback', passport.authenticate('google',
+    {session: false, failureRedirect:'http://localhost:3000/login'}),
+    async(req, res)=> {
+    console.log(req)
+    const user = req.user
+    const userDetails = { email:user.email, id: user._id, firstName:user.firstName, lastName:user.lastName, authorImg:user.authorImg }
+
+    if (user.isTwoFAuthActive) {
+        try {
+
+            const otpCode = generateOTP()
+            user.otp = otpCode
+            const otpExpires = new Date(Date.now() + 10 * 60 * 1000);
+            // console.log(otpExpires)
+            user.otpExpires = otpExpires;
+            await user.save()
+
+            await handleSendEmail('otp.html', user.email, 'OTP Code for 2 Factor Authentication ✅', {
+              username: user.firstName,
+              otpCode: otpCode,
+              year: new Date().getFullYear(),
+            });
+            res.status(200).json({email: user.email, message: "OTP sent successfully!"})
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({error: "Unable to send you an OTP at the moment, please try again later."})
+        }
+    } else {
+        const token = jwt.sign(userDetails, config.CRYPTO_KEY, {expiresIn: '1d'})
+        res.status(200).json({token: token, message: "Logged in successfully!"})
+
+    }
+
+    // res.redirect(`${process.env.CLIENT_URL}/auth/callback?token=${token}`);
 })
 
 loginRouter.post("/validate-otp", async(req, res)=> {
